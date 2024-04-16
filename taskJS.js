fe4c6ecc7c133869211fe242dc41527b21a38682
Alexander Grygoriev <truedevelop@yahoo.com>
2024-04-16 15:13:37 +0300

    next kata owned

--- taskJS.js
+++ taskJS.js
@@ -15,5 +15,17 @@ function createFilledArray(startValue, length ) {
 console.log(createFilledArray(1, 10));
 console.log(createFilledArray(2, 5));
 
+function makeNegative(num) {
+  if(num < 0) return num;
+  if(num === 0 || num === -0) return 0;
+  return num * -1;
+}
+
+console.log(makeNegative(1));    // return -1
+console.log(makeNegative(-5));   // return -5
+console.log(makeNegative(0));    // return 0
+console.log(makeNegative(0.12)); // return -0.12
+
+
 
 })
